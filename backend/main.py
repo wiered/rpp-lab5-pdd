20e4a2e42a546183f94b6721f9354257efd01080
Artem <wieredsh@gmail.com>
2025-06-10 18:54:10 +0300

    changed default port on server

--- backend/main.py
+++ backend/main.py
@@ -19,5 +19,5 @@ if __name__ == "__main__":
     try:
         port = int(os.environ.get("PORT"))
     except:
-        port = 8080
+        port = 8082
     uvicorn.run(app, host="127.0.0.1", port=port, log_level="info")
